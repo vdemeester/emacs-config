37abdf7971f97f4923dd25e06c9966800a617a4c
Vincent Demeester <vincent+git@demeester.fr>
2013-12-09 16:58:29 +0100

    Update few configuration (add ido conf)

--- .emacs.d/el-get.conf.el
+++ .emacs.d/el-get.conf.el
@@ -76,19 +76,19 @@
         '(
          ;; General
          naquadah-theme                ; Theme from Julien Danjou
-          boxquote ; draw boxes
-          multiple-cursors ; multiple cursors
-          ace-jump-mode ; fast cursor movement
-          auto-complete ; universal autocompletion
-          auto-complete-css
-          flx ; fuzzy matching for ido
-          ido-vertical-mode ; vertical mode for ido
-          smex ; IDO for M-x
-          projectile ; handling of projects
-          expand-region ; smartly expand region
+         boxquote ; draw boxes
+         multiple-cursors ; multiple cursors
+         ace-jump-mode ; fast cursor movement
+         auto-complete ; universal autocompletion
+         auto-complete-css
+         flx ; fuzzy matching for ido
+         ido-vertical-mode ; vertical mode for ido
+         smex ; IDO for M-x
+         projectile ; handling of projects
+         expand-region ; smartly expand region
          ;; Programming
          autopair                        ; Auto pairing of parentheses
-          highlight-parentheses ; Highlight parentheses surrounding the cursor
+         highlight-parentheses ; Highlight parentheses surrounding the cursor
          ;; rainbow-mode                        ; Display colors
          dtrt-indent                        ; Autodetect indentation
          magit                                ; Git stuff, must-have!
@@ -97,18 +97,18 @@
          ;; auctex                        ; LaTeX mode
          lua-mode                        ; Major mode for lua
          markdown-mode                        ; Major mode for markdown
-          yaml-mode ; Major mode for YAML
-          go-mode ; Major mode for Go
-          git-commit-mode ; Mode for "git commit"
-          zencoding-mode ; Mode to expand CSS tags to HTML
+         yaml-mode ; Major mode for YAML
+         go-mode ; Major mode for Go
+         git-commit-mode ; Mode for "git commit"
+         zencoding-mode ; Mode to expand CSS tags to HTML
          ;; gnus and other stuff
          ;;bbdb                                ; Big brother database
          ;;nognus                        ; Gnus
          ;;gnus-identities                ; Manipulate Gnus identities
-          ;; org stuff
-          org-mime
-          ;; Misc
-          znc ; znc
-          gist ; gist integration
+         ;; org stuff
+         org-mime
+         ;; Misc
+         znc ; znc
+         gist ; gist integration
          ))
 
