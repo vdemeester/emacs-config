d118be1fd4e7d9e014493ee7926cd858bae8995d
Vincent Demeester <vincent+git@demeester.fr>
2014-01-24 10:43:02 +0100

    Add auto-complete.conf.el :-)

---
+++ .emacs.d/auto-complete.conf.el
@@ -0,0 +1,12 @@
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(setq ac-comphist-file (expand-file-name "run/ac-comphist.dat"
+                                         user-emacs-directory))
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(setq ac-use-quick-help nil) ;; It's slow
+
+;; Don't use up/down arrow (use M-n, M-p only) to browse list
+(define-key ac-completing-map [down] nil)
+(define-key ac-completing-map [up] nil)
