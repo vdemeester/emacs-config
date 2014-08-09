5e267ed860c87a22416b880ad276b9c837ff1c89
Vincent Demeester <vincent+git@demeester.fr>
2014-08-09 14:52:36 +0200

    Few fixes + yes-or-no-p

---
+++ .emacs.d/custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "83edec0673149efea7ecda808ebea37f81b9d165e45f339bf939378371db736e" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
