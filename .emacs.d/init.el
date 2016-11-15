ce1fdd9d85e8649a5e8921abf404ec90afc71b2f
Vincent Demeester <vincent@sbr.pm>
2016-11-15 22:11:31 +0100

    Quick & dirty clean :)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,16 +32,16 @@
   (message "Loaded packages in %.3fs" elapsed))
 
 ;; Make sure we have a decent and recent org-mode version
-;; (unload-org-mode)
-;; (require 'org)
-;; (when (string-match "^[1234567]" (org-version))
-;;   (progn
-;;     (warn "Org-mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version))
-;;     (warn "Force the installation from org elpa.")
-;;     (package-install 'org)
-;;     (unload-org-mode)
-;;     (require 'org)
-;;     ))
+(unload-org-mode)
+(require 'org)
+(when (string-match "^[12345678]" (org-version))
+  (progn
+    (warn "Org-mode is out of date. We expect org 8 or higher, but instead we have %s" (org-version))
+    (warn "Force the installation from org elpa.")
+    (package-install 'org)
+    (unload-org-mode)
+    (require 'org)
+    ))
 
 (setq org-root-directory (substitute-env-in-file-name "$HOME/desktop/org"))
 
