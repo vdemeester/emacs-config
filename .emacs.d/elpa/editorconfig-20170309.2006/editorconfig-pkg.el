c2bf85eba6dd1ea89d0e109bc2b3f90c1624cca4
Vincent Demeester <vincent@sbr.pm>
2017-03-13 10:36:48 +0100

    Update elpa packages
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ .emacs.d/elpa/editorconfig-20170309.2006/editorconfig-pkg.el
@@ -0,0 +1,6 @@
+(define-package "editorconfig" "20170309.2006" "EditorConfig Emacs Plugin"
+  '((cl-lib "0.5"))
+  :url "https://github.com/editorconfig/editorconfig-emacs#readme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/ivy-hydra-20170302.1033/ivy-hydra-autoloads.el
@@ -0,0 +1,15 @@
+;;; ivy-hydra-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ivy-hydra.el") (22725 53736 265815 691000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ivy-hydra-autoloads.el ends here
