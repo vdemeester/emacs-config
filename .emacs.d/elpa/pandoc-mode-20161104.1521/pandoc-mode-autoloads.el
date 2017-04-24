e78d6b9416989aebb8a239b0fed40e0ffb3adc85
Vincent Demeester <vincent@sbr.pm>
2017-04-24 17:10:35 +0200

    Add pandoc mode
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ .emacs.d/elpa/pandoc-mode-20161104.1521/pandoc-mode-autoloads.el
@@ -0,0 +1,33 @@
+;;; pandoc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "pandoc-mode" "pandoc-mode.el" (22782 4330
+;;;;;;  710847 487000))
+;;; Generated autoloads from pandoc-mode.el
+
+(autoload 'pandoc-mode "pandoc-mode" "\
+Minor mode for interacting with Pandoc.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'conditionally-turn-on-pandoc "pandoc-mode" "\
+Turn on pandoc-mode if a pandoc settings file exists.
+This is for use in major mode hooks.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pandoc-mode-pkg.el" "pandoc-mode-utils.el")
+;;;;;;  (22782 4330 757846 31000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pandoc-mode-autoloads.el ends here
