27c7333c350b94a4dc73396b59270c5de78d7f7f
Vincent Demeester <vincent@sbr.pm>
2018-04-05 10:45:05 +0200

    Add a shackle configuration...
    ... not sure if it completely suits me, but will do it interactively.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/vde-windows.el
+++ lisp/vde-windows.el
@@ -75,9 +75,37 @@
          ("C-c w s" . ace-swap-window))
   :config (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
+(use-package shackle
+  :ensure t
+  :config
+  (progn
+    (setq shackle-select-reused-windows nil)
+    (setq shackle-default-alignment 'below)
+    (setq shackle-default-size 0.4)
+
+    (setq shackle-rules
+          '((compilation-mode              :select nil :popup t               :size 0.15             )
+            ("*undo-tree*"                                                    :size 0.25 :align right)
+            ("*eshell*"                    :select t                          :other t               )
+            ("*Shell Command Output*"      :select nil                                               )
+            ("\\*Async Shell.*\\*" :regexp t :ignore t                                                 )
+            (occur-mode                    :select nil                                   :align t    )
+            ("*Help*"                      :select t   :inhibit-window-quit t :other t               )
+            ("*Completions*"                                                  :size 0.3  :align t    )
+            ("*Messages*"                  :select nil :inhibit-window-quit t :other t               )
+            ("\\*[Wo]*Man.*\\*"    :regexp t :select t   :inhibit-window-quit t :other t               )
+            ("\\*poporg.*\\*"      :regexp t :select t                          :other t               )
+            ("\\`\\*helm.*?\\*\\'"   :regexp t                                    :size 0.3  :align t    )
+            ("*Calendar*"                  :select t                          :size 0.3  :align below)
+            ("*info*"                      :select t   :inhibit-window-quit t                         :same t)
+            (magit-status-mode             :select t   :inhibit-window-quit t                         :same t)
+            (magit-log-mode                :select t   :inhibit-window-quit t                         :same t)
+            ))
+    (shackle-mode 1)))
+
 (provide 'vde-windows)
 
 ;; Local Variables:
 ;; coding: utf-8
 ;; indent-tabs-mode: nil
-;; End:
+;; End
