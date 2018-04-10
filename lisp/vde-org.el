3d85a61ca4b02fd57551051aea5939ab5aca7071
Vincent Demeester <vincent@sbr.pm>
2018-04-10 22:58:05 +0200

    Add ox-hugo configuration
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/vde-org.el
+++ lisp/vde-org.el
@@ -2,6 +2,13 @@
   :mode (("\\.org$" . org-mode))
   :ensure org-plus-contrib)
 
+(use-package ox-hugo
+  :ensure t
+  :after ox
+  :commands (org-hugo-slug)
+  :bind (:map modi-mode-map
+         ("C-c G" . org-hugo-export-wim-to-md)))
+
 (provide 'vde-org)
 
 ;; Local Variables:
