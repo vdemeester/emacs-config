aba7af7c0cda29b57204c1f96347a4bb011bf5bc
Vincent Demeester <vincent@sbr.pm>
2018-05-02 19:38:08 +0200

    Add avy and bm
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ lisp/vde-navigating.el
@@ -0,0 +1,32 @@
+(use-package avy                   ; Jump to characters in buffers
+  :bind (("C-c j"   . avy-goto-word-1)
+         ("C-c n b" . avy-pop-mark)
+         ("C-c n j" . avy-goto-char-2)
+         ("C-c n t" . avy-goto-char-timer)
+         ("C-c n w" . avy-goto-word-1)))
+
+(use-package bm
+  :bind (("C-c b b" . bm-toggle)
+         ("C-c b n" . bm-next)
+         ("C-c b p" . bm-previous))
+  :commands (bm-repository-load
+             bm-buffer-save
+             bm-buffer-save-all
+             bm-buffer-restore)
+  :init
+  (add-hook 'after-init-hook 'bm-repository-load)
+  (add-hook 'find-file-hooks 'bm-buffer-restore)
+  (add-hook 'after-revert-hook #'bm-buffer-restore)
+  (add-hook 'kill-buffer-hook #'bm-buffer-save)
+  (add-hook 'after-save-hook #'bm-buffer-save)
+  (add-hook 'vc-before-checkin-hook #'bm-buffer-save)
+  (add-hook 'kill-emacs-hook #'(lambda nil
+                                 (bm-buffer-save-all)
+                                 (bm-repository-save))))
+
+(provide 'vde-navigating)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
