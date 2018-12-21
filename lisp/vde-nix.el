0020627b0e1f73c072d9ad10bffbab185f4baa83
Vincent Demeester <vincent@sbr.pm>
2018-12-21 18:13:47 +0100

    vde-nix: update nix configuration 👼
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/vde-nix.el
+++ lisp/vde-nix.el
@@ -1,11 +1,18 @@
 ;;; -*- lexical-binding: t; -*-
 (use-package nix-mode
-  :mode "\\.nix\\'"
-  :config
-  (use-package nix-repl)
-  (use-package nix-format)
-  (use-package nix-shell
-    :commands (nix-shell nix-unpack)))
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
+
+(use-package nix-format)
 
 (use-package nixos-options)
 
