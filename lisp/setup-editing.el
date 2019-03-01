77c3e6ee8e53d83b372eb0a982652524a6a4d735
Vincent Demeester <vincent@sbr.pm>
2019-03-01 16:11:27 +0100

    setup-editing: enable whitespace in programming mode by default 📿
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-editing.el
+++ lisp/setup-editing.el
@@ -19,6 +19,7 @@
 
 (use-package whitespace
   :defer 1
+  :hook ((prog-mode . whitespace-mode))
   :config
   (setq whitespace-style '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark)))
 
