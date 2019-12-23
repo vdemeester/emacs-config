8845c1726fc40e85a31e12e46d2cc8a6c42692f2
Vincent Demeester <vincent@sbr.pm>
2019-12-23 19:00:43 +0100

    Temporarly remove js-* related modes
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-web.el
+++ lisp/setup-web.el
@@ -36,58 +36,6 @@
         (sp-local-tag "=" "<%= " " %>")
         (sp-local-tag "#" "<%# " " %>")))))
 
-(use-package js2-mode
-  :mode
-  ("\\.js\\'" . js2-mode)
-  :interpreter
-  ("node" . js2-mode))
-
-(use-package js2-refactor
-  :diminish js2-refactor-mode
-  :commands
-  (js2-refactor-mode)
-  :init
-  (add-hook 'js2-mode-hook #'js2-refactor-mode))
-
-(use-package typescript-mode
-  ;; npm install -g typescript
-  :mode
-  ("\\.ts\\'" . typescript-mode)
-  ("\\.ts$\\'" . typescript-mode)
-  :config
-  (setq typescript-enabled-frameworks '(typescript)))
-
-(use-package js-import
-  :commands (js-import js-import-dev))
-
-(use-package rjsx-mode
-  :mode
-  ("\\.jsx?\\'" . rjsx-mode)
-  :interpreter
-  ("node" . rjsx-mode)
-  :config
-  ;; Inspired by http://blog.binchen.org/posts/indent-jsx-in-emacs.html
-  (defun +js-jsx-indent-line-align-closing-bracket ()
-    "Workaround sgml-mode and align closing bracket with opening bracket"
-    (save-excursion
-      (beginning-of-line)
-      (when (looking-at-p "^ +\/?> *$")
-        (delete-char sgml-basic-offset))))
-
-  (advice-add #'js-jsx-indent-line
-              :after
-              #'+js-jsx-indent-line-align-closing-bracket)
-
-  (with-eval-after-load 'rjsx
-    (define-key rjsx-mode-map "<" nil)))
-
-;; (use-package lsp-javascript-typescript
-;;   :hook ((web-mode . lsp-javascript-typescript-enable)
-;;          (js-mode . lsp-javascript-typescript-enable)
-;;          (js3-mode . lsp-javascript-typescript-enable)
-;;          (js2-mode . lsp-javascript-typescript-enable)
-;;          (typescript-mode . lsp-javascript-typescript-enable)))
-
 (provide 'setup-web)
 
 ;; Local Variables:
