36f5f53f67d33eab988554043585dfff8826e40f
Vincent Demeester <vincent@sbr.pm>
2019-12-23 19:37:16 +0100

    setup-windows.el: temporarly disable shackle 🏟
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-windows.el
+++ lisp/setup-windows.el
@@ -67,32 +67,32 @@
          ("C-c w s" . ace-swap-window))
   :config (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
-(use-package shackle
-  :config
-  (progn
-    (setq shackle-select-reused-windows nil)
-    (setq shackle-default-alignment 'below)
-    (setq shackle-default-size 0.20)
+;; (use-package shackle
+;;   :config
+;;   (progn
+;;     (setq shackle-select-reused-windows nil)
+;;     (setq shackle-default-alignment 'below)
+;;     (setq shackle-default-size 0.20)
 
-    (setq shackle-rules
-          '(("*compilation*"               :select nil :popup t               :size 0.15 :align below)
-            ("*undo-tree*"                                                    :size 0.25 :align right)
-            ("*eshell*"                    :select t                          :other t               )
-            ("*Shell Command Output*"      :select nil                                               )
-            ("\\*Async Shell.*\\*" :regexp t :ignore t                                                 )
-            (occur-mode                    :select nil                                   :align t    )
-            ("*Help*"                      :select t   :inhibit-window-quit t :other t               )
-            ("*Completions*"                                                  :size 0.3  :align t    )
-            ("*Messages*"                  :select nil :inhibit-window-quit t :other t               )
-            ("\\*[Wo]*Man.*\\*"    :regexp t :select t   :inhibit-window-quit t :other t               )
-            ("\\*poporg.*\\*"      :regexp t :select t                          :other t               )
-            ("\\`\\*helm.*?\\*\\'"   :regexp t                                    :size 0.3  :align t    )
-            ("*Calendar*"                  :select t                          :size 0.3  :align below)
-            ("*info*"                      :select t   :inhibit-window-quit t                         :same t)
-            (magit-status-mode             :select t   :inhibit-window-quit t                         :same t)
-            (magit-log-mode                :select t   :inhibit-window-quit t                         :same t)
-            ))
-    (shackle-mode 1)))
+;;     (setq shackle-rules
+;;           '(("*compilation*"               :select nil :popup t               :size 0.15 :align below)
+;;             ("*undo-tree*"                                                    :size 0.25 :align right)
+;;             ("*eshell*"                    :select t                          :other t               )
+;;             ("*Shell Command Output*"      :select nil                                               )
+;;             ("\\*Async Shell.*\\*" :regexp t :ignore t                                                 )
+;;             (occur-mode                    :select nil                                   :align t    )
+;;             ("*Help*"                      :select t   :inhibit-window-quit t :other t               )
+;;             ("*Completions*"                                                  :size 0.3  :align t    )
+;;             ("*Messages*"                  :select nil :inhibit-window-quit t :other t               )
+;;             ("\\*[Wo]*Man.*\\*"    :regexp t :select t   :inhibit-window-quit t :other t               )
+;;             ("\\*poporg.*\\*"      :regexp t :select t                          :other t               )
+;;             ("\\`\\*helm.*?\\*\\'"   :regexp t                                    :size 0.3  :align t    )
+;;             ("*Calendar*"                  :select t                          :size 0.3  :align below)
+;;             ("*info*"                      :select t   :inhibit-window-quit t                         :same t)
+;;             (magit-status-mode             :select t   :inhibit-window-quit t                         :same t)
+;;             (magit-log-mode                :select t   :inhibit-window-quit t                         :same t)
+;;             ))
+;;     (shackle-mode 1)))
 
 (defun mu-find-side-windows (&optional side)
   "Get all side window if any.
