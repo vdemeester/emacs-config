0cbe83429deea963c228a4d3c30e638bcaed4cf0
Vincent Demeester <vincent@sbr.pm>
2019-12-23 19:17:03 +0100

    setup-nix.el: clean unused modes
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-nix.el
+++ lisp/setup-nix.el
@@ -9,17 +9,10 @@
 (use-package nix-drv-mode
   :ensure nix-mode
   :mode "\\.drv\\'")
+
 (use-package nix-shell
   :ensure nix-mode
   :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
-(use-package nix-repl
-  :ensure nix-mode
-  :commands (nix-repl))
-
-(use-package nixos-options)
-
-(use-package nix-update
-  :ensure nix-mode)
 
 (provide 'setup-nix)
 
