6024816d572f210d23b31131c14df41f72fa5089
Vincent Demeester <vincent@sbr.pm>
2019-12-23 19:16:50 +0100

    setup-go.el: remove a lot of unused stuff
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-go.el
+++ lisp/setup-go.el
@@ -37,55 +37,9 @@
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet")))
 
-(use-package go-guru
-  :commands (go-guru-describe go-guru-freevars go-guru-implements go-guru-peers
-             go-guru-referrers go-guru-definition go-guru-pointsto
-             go-guru-callstack go-guru-whicherrs go-guru-callers go-guru-callees
-             go-guru-expand-region)
-  :config
-  (unless (executable-find "guru")
-    (warn "go-mode: couldn't find guru, refactoring commands won't work"))
-  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-  (defhydra hydra-go-guru (:color pink :columns 2 :hint nil)
-    "
-^NAME^             ^TYPE^            ^CALL^           ^ALIAS^
-_._: definition    _d_: describe     _lr_: callers     _p_: pointsto
-_r_: referrers     _i_: implement    _le_: callees     _c_: peers
-_f_: freevars      ^ ^               _s_: callstack    _e_: whicherrs"
-    ("." go-guru-definition)
-    ("r" go-guru-referrers)
-    ("f" go-guru-freevars)
-    ("d" go-guru-describe)
-    ("i" go-guru-implements)
-    ("lr" go-guru-callers)
-    ("le" go-guru-callees)
-    ("s" go-guru-callstack)
-    ("p" go-guru-pointsto)
-    ("c" go-guru-peers)
-    ("e" go-guru-whicherrs)
-    ("S" go-guru-set-scope "scope" :color blue)))
-
-(use-package go-eldoc
-  :defer 2
-  :config
-  (add-hook 'go-mode-hook 'go-eldoc-setup))
-
-(use-package gotest
-  :defer 2
-  :after go-mode
-  :bind (:map go-mode-map
-              ("C-c t m" . go-test-current-file)
-              ("C-c t ." . go-test-current-test)
-              ("C-c t c" . go-test-current-coverage)
-              ("C-c t b" . go-test-current-benchmark)))
-
 (use-package flycheck-golangci-lint
   :hook (go-mode . flycheck-golangci-lint-setup)
   :config (setq flycheck-golangci-lint-tests t))
-
-(use-package godoctor
-  :defer 2
-  :after go-mode)
 
 ;; (use-package lsp-go
 ;;   :defer 2
