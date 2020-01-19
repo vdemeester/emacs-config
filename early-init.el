a0140ff6764deebdf23b7b47d639a51404a18d0e
Vincent Demeester <vincent@sbr.pm>
2020-01-19 15:14:18 +0100

    early-init.el: comments and no frame resize
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- early-init.el
+++ early-init.el
@@ -2,7 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 ;;; -*- lexical-binding: t; -*-
+;; Do not initialise the package manager.  This is done in `init.el'.
 (setq package-enable-at-startup nil)
+
+;; Do not resize the frame at this early stage.
+(setq frame-inhibit-implied-resize)
 
 (provide 'early-init)
 ;;; early-init.el ends here
