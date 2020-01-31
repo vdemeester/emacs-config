541794dc0f7e9b8660fd2acc4b7583806fa95400
Vincent Demeester <vincent@sbr.pm>
2020-01-31 14:44:22 +0100

    emacs.org: add setup-notmuch.el
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- lisp/setup-notmuch.el
+++ lisp/setup-notmuch.el
@@ -1,21 +1,17 @@
-;;; setup-notmuch.el --- setup notmuch client -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
-
 (use-package notmuch
   :defer t
   :bind ("<f6>" . notmuch)
   :config
   (setq notmuch-search-oldest-first nil
-	mail-user-agent 'message-user-agent
-	notmuch-tree-show-out t)
+        mail-user-agent 'message-user-agent
+        notmuch-tree-show-out t)
   (setq notmuch-saved-searches
-	'((:key "i" :name "inbox" :query "tag:Inbox")
-	  (:key "r" :name "redhat inbox folder" :query "folder:redhat/Inbox")
-	  (:key "p" :name "perso inbox folder" :query "folder:perso/Inbox")
-	  (:key "u" :name "unread" :query "tag:unread")
-	  (:key "F" :name "flagged" :query "tag:flagged")
-	  (:key "S" :name "sent" :query "tag:Sent Mail"))))
+        '((:key "i" :name "inbox" :query "tag:Inbox")
+          (:key "r" :name "redhat inbox folder" :query "folder:redhat/Inbox")
+          (:key "p" :name "perso inbox folder" :query "folder:perso/Inbox")
+          (:key "u" :name "unread" :query "tag:unread")
+          (:key "F" :name "flagged" :query "tag:flagged")
+          (:key "S" :name "sent" :query "tag:Sent Mail"))))
 
 (use-package sendmail
   :defer t
@@ -23,20 +19,14 @@
   :defines (send-mail-function)
   :config
   (setq send-mail-function 'sendmail-send-it
-	sendmail-program "/home/vincent/bin/msmtp"))
+        sendmail-program "/home/vincent/bin/msmtp"))
 
 (use-package message
   :commands (message-mode message-cite-original-without-signature)
   :config
   (add-hook 'message-mode-hook 'turn-on-auto-fill)
   (setq  message-default-mail-headers "Cc: \nBcc: \n"
-	 message-kill-buffer-on-exit t
-	 message-generate-headers-first t))
+         message-kill-buffer-on-exit t
+         message-generate-headers-first t))
 
 (provide 'setup-notmuch)
-;;; setup-notmuch.el ends here
-
-;; Local Variables:
-;; coding: utf-8
-;; indent-tabs-mode: nil
-;; End:
