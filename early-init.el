aca77b29dc5b8cd82dbea5c2dd6e03edcd953c16
Vincent Demeester <vincent@sbr.pm>
2020-01-31 18:16:04 +0100

    emacs.org: start documenting base-settings…
    We are mainly documenting the early-init.el file and what is the content.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- early-init.el
+++ early-init.el
@@ -1,8 +1,18 @@
-;;; -*- lexical-binding: t; -*-
-;; Do not initialise the package manager.  This is done in `init.el'.
 (setq package-enable-at-startup nil)
 
-;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(add-hook 'after-init-hook
+          `(lambda ()
+             (setq gc-cons-threshold 16777216 ; 16mb
+                   gc-cons-percentage 0.1)
+             (garbage-collect)) t)
 
 (provide 'early-init)
