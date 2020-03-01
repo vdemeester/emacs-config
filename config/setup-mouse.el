43ff45672d5d4e863fc1db88d8af52addf2ffc3c
Vincent Demeester <vincent@sbr.pm>
2020-03-01 19:12:00 +0100

    Update mouse configuration with documentation
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- config/setup-mouse.el
+++ config/setup-mouse.el
@@ -5,4 +5,7 @@
           ((shift) . 5)
           ((meta) . 0.5)
           ((control) . text-scale)))
+  (setq make-pointer-invisible t
+        mouse-wheel-progressive-speed t
+        mouse-wheel-follow-mouse t)
   :hook (after-init . mouse-wheel-mode))
